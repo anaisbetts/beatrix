b21a8bb0d7b2088b7356026f6d3f84055aec75c9
Anaïs Betts <anais@anaisbetts.org>
2025-03-20 21:14:55 +0100

    Why is everything just workinggggg

--- site/App.tsx
+++ site/App.tsx
@@ -16,12 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="bg-primary-800">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="bg-secondary-800">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>

e5b338c60ea0e76fbbcad547399aabe1baf3cda3
Anaïs Betts <anais@anaisbetts.org>
2025-03-20 20:15:06 +0100

    That was the easiest thing I've ever done in my entire fucking life holy shit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,39 @@
 import { configDotenv } from 'dotenv'
-import { connectToHAWebsocket, eventsObservable } from './ha-ws-api'
-import {
-  executePromptWithTools,
-  messagesToString,
-} from './execute-prompt-with-tools'
+import index from '../index.html'
 
 configDotenv()
 
 async function main() {
+  /*
   const connection = await connectToHAWebsocket()
 
-  /*
   const events = eventsObservable(connection)
   events.subscribe((ev) => {
     console.log('Event:', ev)
   })
     */
-
+  /*
   const msgs = await executePromptWithTools(
     connection,
     'Send a notification to the nVidia Shield, with the title "Foo" and the message "bar"'
   )
 
   console.log(messagesToString(msgs))
+  */
+
+  const port = process.env.PORT || '5432'
+
+  console.log('Starting server on port', port)
+  Bun.serve({
+    port: port,
+    routes: {
+      '/': index,
+    },
+  })
 }
 
 main()
-  .then(() => process.exit(0))
+  //.then(() => process.exit(0))
   .catch((err) => {
     console.error(err)
     process.exit(1)

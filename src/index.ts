004e89c0a87fce7ed8d3a59d7a5da1a156b723cf
Anaïs Betts <anais@anaisbetts.org>
2025-03-21 17:46:52 +0100

    Write the crappiest API to expose prompting

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 import { configDotenv } from 'dotenv'
 
 import index from '../site/index.html'
+import {
+  executePromptWithTools,
+  messagesToString,
+} from './execute-prompt-with-tools'
+import { connectToHAWebsocket } from './ha-ws-api'
 
 configDotenv()
 
 async function main() {
   const port = process.env.PORT || '5432'
 
+  const conn = await connectToHAWebsocket()
+
   console.log('Starting server on port', port)
   Bun.serve({
     port: port,
     routes: {
       '/': index,
+      '/api/prompt': {
+        POST: async (req) => {
+          const { prompt } = await req.json()
+          try {
+            const resp = await executePromptWithTools(conn, prompt)
+            const text = messagesToString(resp)
+
+            return Response.json({ prompt, text })
+          } catch (e) {
+            return Response.json({ prompt, error: JSON.stringify(e) })
+          }
+        },
+      },
     },
   })
 }

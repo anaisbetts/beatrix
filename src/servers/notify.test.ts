ab6c9db9a0d35a12049f8bb1005680c7353cc4bd
Anaïs Betts <anais@anaisbetts.org>
2025-03-21 14:33:36 +0100

    Fix

--- src/servers/notify.test.ts
+++ src/servers/notify.test.ts
@@ -26,42 +26,42 @@ describe('the notify server', () => {
             state: 'home',
             attributes: {
               friendly_name: 'Test User',
-              id: 'test_id_123'
-            }
+              id: 'test_id_123',
+            },
           },
           {
             entity_id: 'person.another_user',
             state: 'away',
             attributes: {
-              friendly_name: 'Another User'
-            }
+              friendly_name: 'Another User',
+            },
           },
           {
             entity_id: 'light.bedroom',
             state: 'on',
             attributes: {
-              friendly_name: 'Bedroom Light'
-            }
-          }
+              friendly_name: 'Bedroom Light',
+            },
+          },
         ]
       }
-      
+
       return []
     }
 
     // Get the tool and execute it
     const toolFn = server.getToolMap().get('list-people')!
     const result = await toolFn.fn({})
-    
+
     // Parse the returned content
     const people = JSON.parse(result.content[0].text)
-    
+
     expect(people.length).toBe(2)
     expect(people[0].entity_id).toBe('person.test_user')
     expect(people[0].name).toBe('Test User')
     expect(people[1].entity_id).toBe('person.another_user')
     expect(people[1].name).toBe('Another User')
-    
+
     console.log('People:', people)
   })
 })

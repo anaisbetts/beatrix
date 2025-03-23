090002ca4e726df260061a025f911cdde116e267
Anaïs Betts <anais@anaisbetts.org>
2025-03-23 19:29:37 +0100

    Finally beginning to make even a little bit of progress here

--- site/App.tsx
+++ site/App.tsx
@@ -1,13 +1,74 @@
-import Sidebar from '@/components/sidebar'
 import Chat from '@/components/chat'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuAction,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+  useSidebar,
+} from './components/ui/sidebar'
+import { MessageSquare } from 'lucide-react'
+import { useIsMobile } from './hooks/use-mobile'
+import { useCallback } from 'react'
+
+function AppSidebar() {
+  const { open, isMobile, toggleSidebar } = useSidebar()
+
+  const headerContent =
+    open || isMobile ? (
+      <h2 className="text-2xl text-nowrap">Agentic Automation</h2>
+    ) : null
+
+  const nav = useCallback(() => {
+    if (isMobile) toggleSidebar()
+  }, [isMobile])
+
+  return (
+    <>
+      <SidebarHeader>{headerContent}</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#" onClick={nav}>
+                    <MessageSquare size={18} />
+                    <span className="ms-1">Debug Chat</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </>
+  )
+}
 
 export default function Home() {
+  const defaultOpen = useIsMobile()
+
   return (
-    <div className="bg-background flex h-screen">
-      <Sidebar />
-      <main className="flex-1 overflow-hidden">
-        <Chat />
-      </main>
+    <div className="bg-background h-screen">
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <Sidebar variant="floating" collapsible="icon">
+          <AppSidebar />
+        </Sidebar>
+        <main>
+          <SidebarTrigger />
+          <h2 className="text-2xl">content</h2>
+        </main>
+      </SidebarProvider>
     </div>
   )
 }

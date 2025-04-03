71af2e52815b754ccd456fff07d8c977267e8a63
Anaïs Betts <anais@anaisbetts.org>
2025-04-03 20:45:34 +0200

    Explain this nonsense

--- server/migrations/this-sucks.ts
+++ server/migrations/this-sucks.ts
@@ -1,5 +1,8 @@
 import { MigrationProvider } from 'kysely'
 
+// NB: We do this because Kysely migrators assume that they can roll through
+// a directory of migrators as plain JavaScript files, which isn't true in Bun,
+// in both dev mode and single-file executable mode.
 import * as m1 from './20250323-create'
 import * as m2 from './20250331-add-signal-type-data'
 

813778daf1da52f08bdd2137f527d2f133f6a05c
Anaïs Betts <anais@anaisbetts.org>
2025-04-05 20:16:42 +0200

    Add a repl script

---
+++ scripts/repl.ts
@@ -0,0 +1,12 @@
+import { createDatabase } from '../server/db'
+import { LiveHomeAssistantApi } from '../server/lib/ha-ws-api'
+import { createDefaultLLMProvider } from '../server/llm'
+import { LiveAutomationRuntime } from '../server/workflow/automation-runtime'
+
+export default async function go() {
+  return new LiveAutomationRuntime(
+    await LiveHomeAssistantApi.createViaEnv(),
+    createDefaultLLMProvider(),
+    await createDatabase()
+  )
+}

05552f8f47bf6977697125d7f8eee87434238ccd
Anaïs Betts <anais@anaisbetts.org>
2025-04-07 17:43:04 +0200

    Annotate rscheduling with a bajillion debug lines

--- server/workflow/automation-runtime.ts
+++ server/workflow/automation-runtime.ts
@@ -29,7 +29,7 @@ import { TimerBasedCronScheduler as scheduler } from 'cron-schedule/schedulers/t
 import debug from 'debug'
 import { runExecutionForAutomation } from './execution-step'
 
-const d = debug('ha:service')
+const d = debug('ha:automation')
 
 interface SignalledAutomation {
   signal: Signal
@@ -69,6 +69,10 @@ export class LiveAutomationRuntime implements AutomationRuntime {
   ) {
     this.automationList = []
     this.scheduledTriggers = []
+    d(
+      'Initializing LiveAutomationRuntime with directory: %s',
+      automationDirectory
+    )
 
     this.reparseAutomations = this.automationDirectory
       ? merge(
@@ -79,21 +83,36 @@ export class LiveAutomationRuntime implements AutomationRuntime {
               recursive: true,
             },
             2000
-          ).pipe(map(() => {}))
+          ).pipe(
+            tap(() =>
+              d(
+                'Detected change in automation directory: %s',
+                this.automationDirectory
+              )
+            ),
+            map(() => {})
+          )
         )
       : NEVER
 
     this.scannedAutomationDir = this.automationDirectory
       ? defer(() => this.reparseAutomations).pipe(
           switchMap(() => {
-            d('Reparsing automations...')
-
+            d(
+              'Reparsing automations from directory: %s',
+              this.automationDirectory
+            )
             return from(
               parseAllAutomations(this.automationDirectory!).then(
-                (x) => (this.automationList = x)
+                (automations) => {
+                  d('Parsed %d automations', automations.length)
+                  this.automationList = automations
+                  return automations
+                }
               )
             )
-          })
+          }),
+          share() // Share the result of parsing
         )
       : NEVER
 
@@ -101,34 +120,71 @@ export class LiveAutomationRuntime implements AutomationRuntime {
       () => this.scannedAutomationDir
     ).pipe(
       switchMap((automations) => {
-        d('Rescheduling automations...')
-
-        return from(rescheduleAutomations(this, automations))
-      })
+        d('Rescheduling automations, count: %d', automations.length)
+        return from(rescheduleAutomations(this, automations)).pipe(
+          tap(() => d('Finished rescheduling automations'))
+        )
+      }),
+      share() // Share the result of rescheduling
     )
 
     this.signalFired = defer(() => this.createdSignalsForForAutomations).pipe(
-      switchMap(() => from(this.handlersForDatabaseSignals())),
-      tap({ next: (x) => (this.scheduledTriggers = x) }),
-      switchMap((xs) => merge(...xs.map((x) => x.trigger)))
+      switchMap(() => {
+        d('Setting up triggers based on database signals')
+        return from(this.handlersForDatabaseSignals())
+      }),
+      tap({
+        next: (handlers) => {
+          d('Created %d trigger handlers', handlers.length)
+          this.scheduledTriggers = handlers
+        },
+      }),
+      switchMap((handlers) => {
+        d('Merging %d trigger observables', handlers.length)
+        if (handlers.length === 0) {
+          return NEVER
+        }
+        return merge(...handlers.map((handler) => handler.trigger))
+      }),
+      tap(({ signal, automation }) =>
+        d(
+          'Signal fired for automation %s (%s), signal type: %s, signal ID: %s',
+          automation.hash,
+          automation.fileName,
+          signal.type,
+          signal.id
+        )
+      ),
+      share() // Share the fired signals
     )
 
     this.automationExecuted = defer(() => this.signalFired).pipe(
       switchMap(({ signal, automation }) => {
         d(
-          'Executing automation %s (%s), because %s',
+          'Executing automation %s (%s), triggered by signal %s (ID: %s)',
           automation.hash,
           automation.fileName,
-          signal.type
+          signal.type,
+          signal.id
         )
-
-        return from(runExecutionForAutomation(this, automation, signal.id))
-      })
+        return from(
+          runExecutionForAutomation(this, automation, signal.id)
+        ).pipe(
+          tap(() => d('Finished execution for automation %s', automation.hash))
+        )
+      }),
+      share() // Share the execution result
     )
   }
 
   start() {
-    return this.automationExecuted.subscribe()
+    d('Starting LiveAutomationRuntime event subscription')
+    const subscription = this.automationExecuted.subscribe({
+      error: (err) => d('Error in automation execution pipeline: %o', err),
+      complete: () => d('Automation execution pipeline completed'),
+    })
+    d('Automation execution pipeline subscribed')
+    return subscription
   }
 
   private async handlersForDatabaseSignals(): Promise<TriggerHandler[]> {
@@ -136,52 +192,81 @@ export class LiveAutomationRuntime implements AutomationRuntime {
 
     d('Loading signals from database')
     const signals = await this.db.selectFrom('signals').selectAll().execute()
+    d('Loaded %d signals from database', signals.length)
 
     for (const signal of signals) {
+      d(
+        'Processing signal ID: %s, Type: %s, Hash: %s',
+        signal.id,
+        signal.type,
+        signal.automationHash
+      )
       const automation = this.automationList.find(
         (x) => x.hash === signal.automationHash
       )
 
       if (!automation) {
         d(
-          'Found automation hash %s but not in our list? Deleting',
-          signal.automationHash
+          'Automation hash %s from signal ID %s not found in current automation list. Deleting signal.',
+          signal.automationHash,
+          signal.id
         )
 
         await this.db
           .deleteFrom('signals')
-          .where('automationHash', '=', signal.automationHash)
+          .where('id', '=', signal.id) // Use ID for deletion
           .execute()
+        d('Deleted signal ID: %s', signal.id)
 
         continue
       }
 
-      switch (signal.type) {
-        case 'cron':
-          d('Creating trigger for automation %s', signal.automationHash)
-          triggerHandlers.push(new CronTriggerHandler(signal, automation))
-          break
-        case 'offset':
-          d(
-            'Creating relative time trigger for automation %s',
-            signal.automationHash
-          )
-          triggerHandlers.push(
-            new RelativeTimeTriggerHandler(signal, automation)
-          )
-          break
-        case 'time':
-          d(
-            'Creating absolute time trigger for automation %s',
-            signal.automationHash
-          )
-          triggerHandlers.push(
-            new AbsoluteTimeTriggerHandler(signal, automation)
-          )
-          break
+      d(
+        'Found matching automation %s (%s) for signal ID %s',
+        automation.hash,
+        automation.fileName,
+        signal.id
+      )
+
+      try {
+        switch (signal.type) {
+          case 'cron':
+            d('Creating CronTriggerHandler for signal ID %s', signal.id)
+            triggerHandlers.push(new CronTriggerHandler(signal, automation))
+            break
+          case 'offset':
+            d('Creating RelativeTimeTriggerHandler for signal ID %s', signal.id)
+            triggerHandlers.push(
+              new RelativeTimeTriggerHandler(signal, automation)
+            )
+            break
+          case 'time':
+            d('Creating AbsoluteTimeTriggerHandler for signal ID %s', signal.id)
+            triggerHandlers.push(
+              new AbsoluteTimeTriggerHandler(signal, automation)
+            )
+            break
+          default:
+            d(
+              'Unknown signal type %s for signal ID %s. Skipping.',
+              signal.type,
+              signal.id
+            )
+        }
+      } catch (error) {
+        d(
+          'Error creating trigger handler for signal ID %s: %o',
+          signal.id,
+          error
+        )
+        // Optionally, delete the problematic signal or handle the error differently
       }
     }
 
+    d(
+      'Finished processing signals. Created %d trigger handlers.',
+      triggerHandlers.length
+    )
     return triggerHandlers
   }
 }
@@ -201,18 +286,38 @@ class CronTriggerHandler implements TriggerHandler {
   ) {
     const data: CronTrigger = JSON.parse(signal.data)
     const cron = parseCronExpression(data.cron)
+    d(
+      'CronTriggerHandler created for signal %s, automation %s. Cron: %s',
+      signal.id,
+      automation.hash,
+      data.cron
+    )
     this.trigger = this.cronToObservable(cron).pipe(
-      map(() => ({ signal: this.signal, automation: this.automation }))
+      map(() => {
+        d(
+          'Cron trigger fired for signal %s, automation %s',
+          this.signal.id,
+          this.automation.hash
+        )
+        return { signal: this.signal, automation: this.automation }
+      })
     )
   }
 
   cronToObservable(cron: Cron): Observable<void> {
+    d('Setting up cron interval for: %o', cron)
     return new Observable<void>((subj) => {
-      const handle = scheduler.setInterval(cron, () => {
+      const task = () => {
+        d('Cron task executing for signal %s', this.signal.id)
         subj.next()
-      })
+      }
+      const handle = scheduler.setInterval(cron, task)
+      d('Cron interval scheduled with handle %o', handle)
 
-      return () => scheduler.clearTimeoutOrInterval(handle)
+      return () => {
+        d('Clearing cron interval with handle %o', handle)
+        scheduler.clearTimeoutOrInterval(handle)
+      }
     }).pipe(share())
   }
 }
@@ -226,9 +331,23 @@ class RelativeTimeTriggerHandler implements TriggerHandler {
   ) {
     const relativeTimeData: RelativeTimeTrigger = JSON.parse(signal.data)
     const offsetInSeconds = relativeTimeData.offsetInSeconds
+    d(
+      'RelativeTimeTriggerHandler created for signal %s, automation %s. Offset: %d seconds',
+      signal.id,
+      automation.hash,
+      offsetInSeconds
+    )
 
     this.trigger = timer(offsetInSeconds * 1000).pipe(
-      map(() => ({ signal: this.signal, automation: this.automation }))
+      map(() => {
+        d(
+          'Relative time trigger fired for signal %s, automation %s after %d seconds',
+          this.signal.id,
+          this.automation.hash,
+          offsetInSeconds
+        )
+        return { signal: this.signal, automation: this.automation }
+      })
     )
   }
 }
@@ -244,15 +363,35 @@ class AbsoluteTimeTriggerHandler implements TriggerHandler {
     const targetTime = new Date(absoluteTimeData.iso8601Time).getTime()
     const currentTime = Date.now()
     const timeUntilTarget = targetTime - currentTime
+    d(
+      'AbsoluteTimeTriggerHandler created for signal %s, automation %s. Target time: %s',
+      signal.id,
+      automation.hash,
+      absoluteTimeData.iso8601Time
+    )
 
     // Only schedule if the time is in the future
     if (timeUntilTarget > 0) {
+      d(
+        'Scheduling absolute time trigger for signal %s in %d ms',
+        signal.id,
+        timeUntilTarget
+      )
       this.trigger = timer(timeUntilTarget).pipe(
-        map(() => ({ signal: this.signal, automation: this.automation }))
+        map(() => {
+          d(
+            'Absolute time trigger fired for signal %s, automation %s at %s',
+            this.signal.id,
+            this.automation.hash,
+            absoluteTimeData.iso8601Time
+          )
+          return { signal: this.signal, automation: this.automation }
+        })
       )
     } else {
       d(
-        'Skipping past-due absolute time trigger: %s',
+        'Skipping past-due absolute time trigger for signal %s: Target time %s is in the past.',
+        signal.id,
         absoluteTimeData.iso8601Time
       )
       this.trigger = NEVER
